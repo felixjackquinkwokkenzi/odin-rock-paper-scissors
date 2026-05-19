43eecee3d06886680efe38bbf8675fcf60e5bfe5
felixjackquinkwokkenzi <kwokfelix2@gmail.com>
2026-05-19 17:11:23 +0700

    Implement playRound logic and convert computer choices to strings

--- script.js
+++ script.js
@@ -4,10 +4,42 @@ let computerScore = 0;
 
 function getComputerChoice() {
   const random = Math.floor(Math.random() * choice) + 1;
-  return random;
+  if (random === 1) {
+    return "rock";
+  } else if (random === 2) {
+    return "paper";
+  } else if (random === 3) {
+    return "scissors";
+  }
 }
 
 function getHumanChoice() {
   let choice = prompt("Please enter rock, paper, or scissors");
-  return choice;
+  return choice ? choice.toLowerCase() : "";
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You Lose! Paper beats Rock");
+    computerScore++;
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You Win! Rock beats Scissors");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You Win! Paper beats Rock");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You Lose! Scissors beats Paper");
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You Lose! Rock beats Scissors");
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You Win! Scissors beats Paper");
+    humanScore++;
+  } else if (humanChoice === computerChoice) {
+    console.log("It's a Tie!");
+  } else {
+    console.log("Invalid input!");
+  }
 }
